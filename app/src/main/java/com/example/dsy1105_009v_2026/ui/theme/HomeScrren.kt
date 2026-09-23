ef0a17c6e673dde114e8ab501065eb0a8fb36da6
PAAWSA03LC0700 <vice.zapata@profesor.duoc.cl>
2026-09-23 19:54:07 -0300

    aaa

---
+++ app/src/main/java/com/example/dsy1105_009v_2026/ui/theme/HomeScrren.kt
@@ -0,0 +1,58 @@
+package com.example.dsy1105_009v_2026.ui.theme
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun HomeScreen(){
+// Scaffold: permite generar una pantalla basica
+    Scaffold (
+        topBar={
+            TopAppBar(title={ Text("Mi Primer App")})
+        }// fin topBar
+
+
+    ){ innerPadding ->
+        //innerPadding  es un espacio interno que entrega Scaffold
+        Column(
+            modifier= Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ){
+            Text(text="!!!  Bienvenido !!!!!!")
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+}// fin HomeScreen
+
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen()
+}
