e86e2a8f1b7afc0861cbe9a3e02d315d2b5a7d23
PAAWSA03LC0700 <vice.zapata@profesor.duoc.cl>
2026-09-23 20:16:04 -0300

    aaa

--- app/src/main/java/com/example/dsy1105_009v_2026/MainActivity.kt
+++ app/src/main/java/com/example/dsy1105_009v_2026/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dsy1105_009v_2026.ui.theme.DSY1105009V2026Theme
+import com.example.dsy1105_009v_2026.ui.theme.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +21,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             DSY1105009V2026Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+
+
+                    //Greeting(
+                    //    name = "Android",
+                    //    modifier = Modifier.padding(innerPadding)
+                    //)
+                    HomeScreen()
+
+
                 }
             }
         }
--- app/src/main/java/com/example/dsy1105_009v_2026/ui/theme/HomeScrren.kt
+++ app/src/main/java/com/example/dsy1105_009v_2026/ui/theme/HomeScrren.kt
@@ -1,17 +1,24 @@
 package com.example.dsy1105_009v_2026.ui.theme
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.dsy1105_009v_2026.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,16 +39,24 @@ fun HomeScreen(){
                 .fillMaxSize()
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(20.dp)
-        ){
+        ){  //inicio inner
             Text(text="!!!  Bienvenido !!!!!!")
-
-        }
-
-
-
+            Button(onClick={/*  accion futura */}){
+                Text("Presioname")
+            }// fin text
 
 
+            Image(
+                painter= painterResource(id= R.drawable.logoduoc),
+                contentDescription = "Logo Appp",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
 
+            )
+
+        }//fin inner
 
 
     }
