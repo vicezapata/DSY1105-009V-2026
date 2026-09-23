e55d2b2d6bfe4c2c296f0f59d7a63d9c07c34b16
PAAWSA03LC0700 <vice.zapata@profesor.duoc.cl>
2026-09-23 20:59:08 -0300

    aaa

--- app/src/main/java/com/example/dsy1105_009v_2026/ui/theme/HomeScrren.kt
+++ app/src/main/java/com/example/dsy1105_009v_2026/ui/theme/HomeScrren.kt
@@ -1,21 +1,30 @@
 package com.example.dsy1105_009v_2026.ui.theme
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.dsy1105_009v_2026.R
@@ -24,10 +33,26 @@ import com.example.dsy1105_009v_2026.R
 @Composable
 
 fun HomeScreen(){
+
+    val ColorScheme= darkColorScheme(
+        primary = Color(0xFF98222E),
+        onPrimary = Color.White,
+        onSurface = Color(0xFF333333), //gris
+    )
+
+    MaterialTheme(
+        colorScheme = ColorScheme
+    ){ // Inicio aplicacion Material
+
+
+
+
 // Scaffold: permite generar una pantalla basica
     Scaffold (
         topBar={
-            TopAppBar(title={ Text("Mi Primer App")})
+            TopAppBar(title={ Text("Mi Primer App",
+                color= MaterialTheme.colorScheme.onPrimary
+            )})
         }// fin topBar
 
 
@@ -37,14 +62,20 @@ fun HomeScreen(){
             modifier= Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(20.dp)
-        ){  //inicio inner
-            Text(text="!!!  Bienvenido !!!!!!")
-            Button(onClick={/*  accion futura */}){
-                Text("Presioname")
-            }// fin text
+                .padding(16.dp)
+                .background(Color(0xFFF0F0F0)),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
 
+
+        ){  //inicio inner
+            Text(text="!!!  Bienvenido !!!!!!",
+                style= MaterialTheme.typography.headlineMedium,
+                color=MaterialTheme.colorScheme.primary
+                )
+
+            Spacer(modifier= Modifier.height(16.dp)
+            )
 
             Image(
                 painter= painterResource(id= R.drawable.logoduoc),
@@ -56,12 +87,47 @@ fun HomeScreen(){
 
             )
 
+
+            Row(
+                modifier= Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal=16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+            ){// Aplicar fila
+
+                Text("Texto uno",
+                    style= MaterialTheme.typography.bodyLarge.copy(
+                        color= MaterialTheme.colorScheme.onSurface.copy(alpha=0.8f),
+                        fontWeight = FontWeight.Bold),
+                    modifier= Modifier
+                        .padding(end=8.dp)
+
+                    )//fin Text1
+
+
+            }// fin Aplicar fila
+
+
+
+            Spacer(modifier= Modifier
+                .height(16.dp)
+            )
+
+            Button(onClick={/*  accion futura */}){
+                Text("Presioname")
+            }// fin text
+
+
+
+
         }//fin inner
 
 
-    }
+    } // Fin Columna
 
 
+
+    } // Final aplicacion Material
 
 }// fin HomeScreen
 
